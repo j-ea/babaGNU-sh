6d55120e488fbec0fdbecc863ca5deb3d9de4741
j-ea <janderson73@gmail.com>
2020-05-24 08:40:42 -0700

    Fix build error - gatsby-plugin-manifest error 11321

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `/content/assets/polyplant.png`,
+        //icon: `/content/assets/polyplant.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
